796be75d1a66d60095832c41df991fadd8cf3b1b
ibaby820929-creator <ibaby820929@gmail.com>
2026-04-01 20:13:21 +0800

    Refactor data fetching and error handling in sina.js

--- api/sina.js
+++ api/sina.js
@@ -9,8 +9,8 @@ function fetchUrl(url) {
       resp.on('data', c => chunks.push(c));
       resp.on('end', () => {
         const buf = Buffer.concat(chunks);
-        const decoder = new TextDecoder('gbk');
-        resolve(decoder.decode(buf));
+        try { resolve(new TextDecoder('gbk').decode(buf)); }
+        catch(e) { resolve(buf.toString()); }
       });
     }).on('error', () => resolve(''));
   });
@@ -19,26 +19,31 @@ function fetchUrl(url) {
 module.exports = async (req, res) => {
   const { code, detail } = req.query;
   if (!code) return res.status(400).json({ error: 'missing code' });
-
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
   const hqText = await fetchUrl(`https://hq.sinajs.cn/rn=${Date.now()}&list=${code}`);
 
   if (detail === '1') {
-    const fText = await fetchUrl(`https://finance.sina.com.cn/realstock/company/${code}/jsvar.js`);
-    let pe = '', pb = '', totalMv = '', turnover = '', eps = '';
-    const peM = fText.match(/pe_d[^=]*=\s*"?([0-9.]+)/);
-    const pbM = fText.match(/pb[^=]*=\s*"?([0-9.]+)/);
-    const mvM = fText.match(/totalMarketCap[^=]*=\s*"?([0-9.]+)/);
-    const toM = fText.match(/turnover[^=]*=\s*"?([0-9.]+)/);
-    if (peM) pe = peM[1];
-    if (pbM) pb = pbM[1];
-    if (mvM) totalMv = mvM[1];
-    if (toM) turnover = toM[1];
+    const rawCode = code.replace(/^sh|^sz/, '');
+    const market = code.startsWith('sh') ? 'sh' : 'sz';
+    const fUrl = `https://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=${code}&scale=240&ma=5&datalen=1`;
+    const iUrl = `http://push2.eastmoney.com/api/qt/stock/get?secid=${market === 'sh' ? '1' : '0'}.${rawCode}&ut=fa5fd1943c7b386f172d6893dbfba10b&fltt=2&invt=2&fields=f162,f167,f168,f116,f117`;
+    
+    let pe = '', pb = '', totalMv = '', turnover = '';
+    try {
+      const iText = await fetchUrl(iUrl);
+      const iJson = JSON.parse(iText);
+      if (iJson && iJson.data) {
+        pe = iJson.data.f162 || '';
+        pb = iJson.data.f167 || '';
+        turnover = iJson.data.f168 || '';
+        totalMv = iJson.data.f116 || '';
+      }
+    } catch(e) {}
+
     res.status(200).json({ hq: hqText, pe, pb, totalMv, turnover });
   } else {
     res.status(200).send(hqText);
   }
 };
-
